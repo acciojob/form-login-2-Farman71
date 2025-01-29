97a6868491f35043df58380fab536531bc2563cd
Farman71 <farmanhussain71@gmail.com>
2025-01-29 18:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getFormvalue() {
+	let fName=document.getElementById("fname");
+	let lName=document.getElementById("lname");
+	let pNum=document.getElementById("num");
+	let eMail=document.getElementById("email");
+	let string=`First Name: ${fName.value} Last Name: ${lName.value} Phone Number: ${pNum.value} Email ID: ${eMail.value}`
+	alert(string)
+}
